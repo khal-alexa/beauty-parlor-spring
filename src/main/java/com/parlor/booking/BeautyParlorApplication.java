9ec916c5ac4efd40f0786233030dc7f491205e8e
khal-alexa <oleksandra.khalaim@gmail.com>
2020-09-25 15:33:47 +0300

    Initial commit

---
+++ src/main/java/com/parlor/booking/BeautyParlorApplication.java
@@ -0,0 +1,13 @@
+package com.parlor.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautyParlorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeautyParlorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/parlor/booking/BeautyParlorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parlor.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautyParlorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
