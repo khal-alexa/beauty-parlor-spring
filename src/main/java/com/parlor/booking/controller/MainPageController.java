19718b42886f7fd592ff6ef5ac8e1d5cc8efcb38
khal-alexa <oleksandra.khalaim@gmail.com>
2020-09-30 10:57:34 +0300

    BSPR-8 add logging

--- src/main/java/com/parlor/booking/controller/MainPageController.java
+++ src/main/java/com/parlor/booking/controller/MainPageController.java
@@ -1,5 +1,6 @@
 package com.parlor.booking.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@Log4j2
 public class MainPageController {
     @GetMapping("/")
     public ModelAndView mainPage() {
@@ -24,11 +26,12 @@ public class MainPageController {
     public ModelAndView afterLogin(@RequestParam(value = "error", required = false) String error) {
         ModelAndView model = new ModelAndView();
         if (error != null) {
+            log.error("Invalid Credentials provided.");
             model.addObject("loginError", "Invalid Credentials provided.");
             model.setViewName("error");
             return model;
         }
-
+        log.info("Authorization successful");
         model.setViewName("client");
         return model;
     }
