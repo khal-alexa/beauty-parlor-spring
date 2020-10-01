1f5d3c32fdc5a9986e07a9598c4a30c416f423ca
khal-alexa <oleksandra.khalaim@gmail.com>
2020-10-01 19:41:33 +0300

    BSPR-10 add exception handler

--- src/main/java/com/parlor/booking/service/UserService.java
+++ src/main/java/com/parlor/booking/service/UserService.java
@@ -4,12 +4,11 @@ import com.parlor.booking.domain.UserDto;
 import com.parlor.booking.entity.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.util.Optional;
 
 public interface UserService extends UserDetailsService {
-    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+    UserDetails loadUserByUsername(String username);
 
     boolean addNewUser(UserDto userDto);
 
