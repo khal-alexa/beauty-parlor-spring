14d6fb7fa818f588ffb311398fc195675faa23f4
khal-alexa <oleksandra.khalaim@gmail.com>
2020-10-03 23:59:45 +0300

    BSPR-16 add feedback validation

--- src/main/java/com/parlor/booking/domain/FeedbackDto.java
+++ src/main/java/com/parlor/booking/domain/FeedbackDto.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
 import java.time.LocalDateTime;
 
 @Data
@@ -14,7 +16,11 @@ import java.time.LocalDateTime;
 public class FeedbackDto {
     private Long id;
     private int rate;
+
+    @NotNull(message = "Feedback should not be empty")
+    @Length(min = 1, max = 2000)
     private String text;
+
     private Long clientId;
     private String specialistName;
     private LocalDateTime createdOn;
